8dfee89a65ac506b88d3cea3733abb0f499a98e1
Abdallah Askar <abdoasker.1426@gmail.com>
2023-08-31 01:50:17 +0300

    detailed documentation of the API using Swagger📄

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import app from './app.js';
 const PORT = process.env.PORT || 3000;
 const server= createServer(app)
 
+import swaggerUi from 'swagger-ui-express';
+// const swaggerFile = await import('./swagger-output.json');
+import swaggerFile from './swagger-output.json' assert { type: 'json' };
 
 import { pool } from './database/db.js';
 
@@ -14,16 +17,16 @@ const waitForDatabase = async () => {
       console.log('Database connected');
       break; 
     } catch (error) {
-      console.log(error)
+      // console.log(error)
       console.log('Waiting for database...');
-      await new Promise(resolve => setTimeout(resolve, 1000));
+      await new Promise(resolve => setTimeout(resolve, 10000));
     }
   }
 };
 
 const startServer = async () => {
   await waitForDatabase(); 
- 
+  app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
   server.listen(PORT, () => {
     console.log(`#server is running on port ${PORT}`);
   });
